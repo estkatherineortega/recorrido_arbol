a13efb5f895754efa8e40fdb5cb635c9371de81e
Katherine Ortega <estkatherineortega>
2026-05-15 21:07:49 -0500

    Codigo completado

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
 
+//KATHERINE ORTEGA CONTRERAS//
 public class Main {
 
     public static void main(String[] args) {
-        NodoArbol tree = new NodoArbol<Integer>();
+
+        NodoArbol<Integer> tree = new NodoArbol<>();
         tree.setDato(45);
-        
-        //tree.setHijo(new NodoArbol[] {new NodoArbol(23, null), new NodoArbol(11, null)});
-        NodoArbol[] arrNodoArbol = { new NodoArbol(23, null), new NodoArbol(11, null)};
+
+        NodoArbol[] arrNodoArbol = {
+            new NodoArbol(23, null),
+            new NodoArbol(11, null)
+        };
+
         tree.setHijo(arrNodoArbol);
-        tree.preOrden();
+
         tree.getHijo()[0].setHijo(0, 50);
         tree.getHijo()[1].setHijo(0, 40);
+
+        System.out.println("Recorrido PreOrden:");
+        tree.preOrden();
+
+        System.out.println("\nRecorrido InOrden:");
+        tree.inOrden();
+
+        System.out.println("\nRecorrido PostOrden:");
+        tree.postOrden();
     }
-    
 }
-//KATHERINE ORTEGA CONTRERAS//
--- src/NodoArbol.java
+++ src/NodoArbol.java
@@ -1,4 +1,5 @@
 public class NodoArbol<T> {
+
     private T dato;
     private NodoArbol[] hijo;
 
@@ -25,17 +26,54 @@ public class NodoArbol<T> {
     public void setHijo(NodoArbol[] hijo) {
         this.hijo = hijo;
     }
-    
+
     public void setHijo(int pos, T dato) {
-        
+        if (hijo == null) {
+            hijo = new NodoArbol[pos + 1];
+        }
+        hijo[pos] = new NodoArbol(dato, null);
     }
 
+    // PREORDEN
     public void preOrden() {
-        System.out.print(this.dato);
+        System.out.print(this.dato + " ");
+
         if (hijo == null) return;
+
         for (NodoArbol n : hijo) {
-            System.out.print(", ");
-            n.preOrden();
+            if (n != null) {
+                n.preOrden();
+            }
         }
+    }
+
+    
+    public void inOrden() {
+        if (hijo != null && hijo.length > 0 && hijo[0] != null) {
+            hijo[0].inOrden();
+        }
+
+        System.out.print(this.dato + " ");
+
+        if (hijo != null) {
+            for (int i = 1; i < hijo.length; i++) {
+                if (hijo[i] != null) {
+                    hijo[i].inOrden();
+                }
+            }
+        }
+    }
+
+    
+    public void postOrden() {
+        if (hijo != null) {
+            for (NodoArbol n : hijo) {
+                if (n != null) {
+                    n.postOrden();
+                }
+            }
+        }
+
+        System.out.print(this.dato + " ");
     }
 }
